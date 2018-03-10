3eaa1f93398ce246b7bdc5835e89dacb07aab434
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-03-10 22:57:09 +0900

    commit

---
+++ NumpyPrac.py
@@ -0,0 +1,17 @@
+'''
+Created on 2018. 3. 10.
+
+@author: Insup Jung
+'''
+import numpy as np
+if __name__ == '__main__':
+    
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+    print(a+b)
+    
+    c = [1, 2, 3]
+    d = [4, 5, 6]
+    print(c+d)
+    
+    pass
