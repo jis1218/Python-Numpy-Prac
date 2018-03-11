d657e9a8b02defff5ae5c47bfe4ecd0206f1fb34
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-03-11 23:32:35 +0900

    commit

--- NumpyPrac.py
+++ NumpyPrac.py
@@ -8,10 +8,15 @@ if __name__ == '__main__':
     
     a = np.array([1, 2, 3])
     b = np.array([4, 5, 6])
-    print(a+b)
+    print(a+b) #element-wise
     
     c = [1, 2, 3]
     d = [4, 5, 6]
     print(c+d)
+    print(3+b) #Broadcast
     
+    e = np.array([[1, 2], [1, 2]])
+    f = np.array([[3, 4], [3, 4]])
+    print(e+f)
+    print(e*f)
     pass
