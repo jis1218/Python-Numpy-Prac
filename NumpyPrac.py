15ce96e546112d5364c1ad5436aef6380c685990
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-03-12 16:30:29 +0900

    commit

--- NumpyPrac.py
+++ NumpyPrac.py
@@ -4,7 +4,11 @@ Created on 2018. 3. 10.
 @author: Insup Jung
 '''
 import numpy as np
+from dask.array.tests.test_numpy_compat import dtype
 if __name__ == '__main__':
+    
+    s = "hello"
+    print(s[0])
     
     a = np.array([1, 2, 3])
     b = np.array([4, 5, 6])
@@ -19,4 +23,8 @@ if __name__ == '__main__':
     f = np.array([[3, 4], [3, 4]])
     print(e+f)
     print(e*f)
+    print(np.dot(e,f))
+
+    
+    
     pass
